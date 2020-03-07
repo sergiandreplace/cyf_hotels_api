00f6274ffbff37c414a127ba37e9f19bc3718305
Sergi Martínez <sergi.martinez@scmspain.com>
2020-03-07 13:15:03 +0100

    get hotels endpoint

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 const express = require("express");
 const app = express();
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'cyf_hotels',
+    password: 'firefox',
+    port: 5432
+});
+
+app.get("/hotels", function(req, res) {
+    pool.query('SELECT * FROM hotels', (error, result) => {
+        res.json(result.rows);
+    });
+});
+
 
 app.listen(3000, function() {
     console.log("Server is listening on port 3000. Ready to accept requests!");
