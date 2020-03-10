c9c062d378b65dac9746b21c554b251ef8d8eaa7
Sergi Martínez <sergi.martinez@gmail.com>
2020-03-10 17:59:31 +0100

    doc formatted

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 const express = require("express");
 const app = express();
 const { Pool } = require("pg");
-const secrets = require('./secrets');
+const secrets = require("./secrets");
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'cyf_hotels',
-    password: secrets.dbPassword,
-    port: 5432
+  user: "postgres",
+  host: "localhost",
+  database: "cyf_hotels",
+  password: secrets.dbPassword,
+  port: 5432
 });
 
-console.log(secrets)
+console.log(secrets);
 
 app.get("/hotels", function(req, res) {
   pool.query("SELECT * FROM hotels", (error, result) => {
     if (error) {
       res.status(500).send(error);
     } else {
-    res.json(result.rows);
+      res.json(result.rows);
     }
   });
 });
