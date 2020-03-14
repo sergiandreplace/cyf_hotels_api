358ced2d80e830344bbc686264c97b46ea3494a9
Sergi Martínez <sergi.martinez@gmail.com>
2020-03-14 11:18:45 +0100

    post hotels created

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const { Pool } = require("pg");
+const bodyParser = require("body-parser");
 const secrets = require("./secrets");
 
 const pool = new Pool({
@@ -11,16 +12,38 @@ const pool = new Pool({
   port: 5432
 });
 
-console.log(secrets);
+app.use(bodyParser.json());
 
 app.get("/hotels", function(req, res) {
-  pool.query("SELECT * FROM hotels", (error, result) => {
-    if (error) {
-      res.status(500).send(error);
-    } else {
-      res.json(result.rows);
-    }
-  });
+  pool
+    .query("SELECT * FROM hotels")
+    .then(result => res.json(result.rows))
+    .catch(err => res.status(500).send(error));
+});
+
+app.post("/hotels", function(req, res) {
+  const newHotelName = req.body.name;
+  const newHotelRooms = req.body.rooms;
+  const newHotelPostcode = req.body.postcode;
+
+  if (!Number.isInteger(newHotelRooms) || newHotelRooms <= 0) {
+    return res
+      .status(400)
+      .send("The number of rooms should be a positive integer.");
+  }
+
+  pool
+    .query("SELECT * FROM hotels WHERE name=$1", [newHotelName])
+    .then(result => {
+      if (result.rows.length > 0) {
+        return res.status(400).send("An hotel with the same name already exists!");
+      } else {
+        const query = "INSERT INTO hotels (name, rooms, postcode) VALUES ($1, $2, $3)";
+        pool.query(query, [newHotelName, newHotelRooms, newHotelPostcode])
+          .then(() => res.send("Hotel created!"))
+          .catch(e => res.status(500).send(e));
+      }
+    });
 });
 
 app.listen(3000, function() {
