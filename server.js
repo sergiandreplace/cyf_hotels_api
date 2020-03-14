17e0093eb292c0d2d56acc8c74ded65aee1ba5bf
Sergi Martínez <sergi.martinez@gmail.com>
2020-03-14 14:10:14 +0100

    added customer name query param

--- server.js
+++ server.js
@@ -40,37 +40,21 @@ app.get("/hotels/:hotelId", (req, res) => {
 
 })
 
-app.post("/hotels", function(req, res) {
-  const newHotelName = req.body.name;
-  const newHotelRooms = req.body.rooms;
-  const newHotelPostcode = req.body.postcode;
+app.get("/customers", function(req, res) {
+  const customerNameQuery = req.query.name
 
-  if (!Number.isInteger(newHotelRooms) || newHotelRooms <= 0) {
-    return res
-      .status(400)
-      .send("The number of rooms should be a positive integer.");
+  let query = "SELECT * FROM customers ORDER BY name"
+
+  if (customerNameQuery) {
+    query = `SELECT * FROM customers WHERE name ilike '%${customerNameQuery}%' ORDER BY name `
   }
 
   pool
-    .query("SELECT * FROM hotels WHERE name=$1", [newHotelName])
-    .then(result => {
-      if (result.rows.length > 0) {
-        return res.status(400).send("An hotel with the same name already exists!");
-      } else {
-        const query = "INSERT INTO hotels (name, rooms, postcode) VALUES ($1, $2, $3)";
-        pool.query(query, [newHotelName, newHotelRooms, newHotelPostcode])
-          .then(() => res.send("Hotel created!"))
-          .catch(e => res.status(500).send(e));
-      }
-    });
-});
-
-app.get("/customers", function(req, res) {
-  pool
-    .query("SELECT * FROM customers")
+    .query(query)
     .then(result => res.json(result.rows))
     .catch(err => res.status(500).send(error));
 });
+
 
 
 app.post("/customers", function(req, res) {
