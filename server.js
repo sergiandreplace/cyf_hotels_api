c31ebe987a042f6f91cc5812da92b30876a8120f
Sergi Martínez <sergi.martinez@gmail.com>
2020-03-14 12:51:49 +0100

    get with query params and hotel id

--- server.js
+++ server.js
@@ -15,11 +15,30 @@ const pool = new Pool({
 app.use(bodyParser.json());
 
 app.get("/hotels", function(req, res) {
+  const hotelNameQuery = req.query.name;
+  console.log(`query name: ${hotelNameQuery}`)
+
+  let query = `SELECT * FROM hotels ORDER BY name`;
+
+  if(hotelNameQuery) {
+    query = `SELECT * FROM hotels WHERE name ilike '%${hotelNameQuery}%' ORDER BY name`
+  }
+  console.log(query)
+
   pool
-    .query("SELECT * FROM hotels")
+    .query(query)
     .then(result => res.json(result.rows))
     .catch(err => res.status(500).send(error));
 });
+
+app.get("/hotels/:hotelId", (req, res) => {
+  const hotelId = req.params.hotelId;
+
+  pool.query("SELECT * FROM hotels WHERE id=$1", [hotelId])
+    .then(result => res.json(result.rows))
+    .catch(e => console.error(e));
+
+})
 
 app.post("/hotels", function(req, res) {
   const newHotelName = req.body.name;
@@ -62,12 +81,20 @@ app.post("/customers", function(req, res) {
   const newPostcode = req.body.postcode;
   const newCountry = req.body.country;
 
-  const query = "INSERT INTO customers (name, email, address, city, postcode, country) VALUES ($1, $2, $3, $4, $5, $6)"
-  const params = [newName, newEmail, newAddress, newCity, newPostcode, newCountry];
+  pool.query("SELECT * FROM customers WHERE name = $1", [newName])
+    .then(result => {
+      if (result.rows.length > 0) {
+        return res.status(400).send("A customer with the same name already exists!");
+      } else {
+        const query = "INSERT INTO customers (name, email, address, city, postcode, country) VALUES ($1, $2, $3, $4, $5, $6)"
+        const params = [newName, newEmail, newAddress, newCity, newPostcode, newCountry];
+      
+        pool.query(query, params)
+          .then(() => res.send("Customer created"))
+          .catch(e => res.status(500).send(e))
+      }
+    })
 
-  pool.query(query, params)
-    .then(() => res.send("Customer created"))
-    .catch(e => res.status(500).send(e))
 })
 
 app.listen(3000, function() {
